f16994ce2ff75ba100d1652b7260d37783e6d7e6
Thomas Poth <thomas.poth@gmail.com>
2022-07-07 10:16:59 +0200

    use http module as server
    while adding express as a handler for http

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+"use strict";
+
+const http = require("http");
+
 const express = require("express");
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+const server = http.createServer(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
