518de79972ef28c76b57ba582cd4f9f3bdc0171f
Thomas Poth <thomas.poth@gmail.com>
2022-07-07 10:25:49 +0200

    send json array at /people endpoint

--- app.js
+++ app.js
@@ -8,12 +8,37 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  const hello = "Hallo Welt!";
+
+  res.send(hello);
+});
+
+app.get("/person", (req, res) => {
   const person = {
     firstName: "Jane",
     lastName: "Doe",
   };
 
   res.send(person);
+});
+
+app.get("/people", (req, res) => {
+  const people = [
+    {
+      firstName: "Jane",
+      lastName: "Doe",
+    },
+    {
+      firstName: "John",
+      lastName: "Done",
+    },
+    {
+      firstName: "James",
+      lastName: "Doer",
+    },
+  ];
+
+  res.send(people);
 });
 
 const server = http.createServer(app);
